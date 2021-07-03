1f98316d3b9f3ee8a158c475321530137a89faaa
Dinesh-Gautam <dineshgautam5252@gmail.com>
2021-07-03 18:30:25 +0530

    search result is saved with a paragraph index

--- dev/mainApp/main.js
+++ dev/mainApp/main.js
@@ -1,5 +1,5 @@
 let data = null;
-let filteredData = "";
+let filteredData = [];
 
 fetch("../recognized.json", { method: "GET", type: "JSON" })
   .then((e) => e.json())
@@ -21,9 +21,11 @@ function modifyData() {
   data.forEach((e) => {
     let filteredString = e.text.replace(/[|&;$%@"<>()+,]/g, " ");
     filteredString = filteredString.replace(/\n/g, " ");
-    filteredData += ` ${filteredString}`;
+    filteredData.push(` ${filteredString}`);
   });
 }
+const finalAnswer = [];
+let searchingDone = false;
 
 function findQuestionsAnswers(model) {
   console.log("Model Loaded!");
@@ -31,22 +33,57 @@ function findQuestionsAnswers(model) {
     .querySelector(".question-searcher-btn")
     .addEventListener("click", (e) => {
       e.preventDefault();
-      model
-        .findAnswers(
-          document.getElementById("questionInput").value,
-          filteredData
-        )
-        .then((answers) => {
-          const resultElement = document.querySelector(".result");
-          resultElement.innerHTML = "";
-          const ul = document.createElement("ul");
-          answers.forEach((e) => {
-            const li = document.createElement("li");
-            li.innerText = e.text;
-            ul.appendChild(li);
-          });
 
-          resultElement.appendChild(ul);
-        });
+      filteredData.forEach((string, index) => {
+        model
+          .findAnswers(document.getElementById("questionInput").value, string)
+          .then((answers) => {
+            if (answers.length > 0) {
+              answers.forEach((e) => {
+                e.index = index;
+                finalAnswer.push(e);
+              });
+            }
+            if (index + 1 === filteredData.length) {
+              searchingDone = true;
+            } else {
+              searchingDone = false;
+            }
+            allParagraphsSearched();
+          });
+      });
     });
 }
+function allParagraphsSearched() {
+  if (!searchingDone) {
+    return;
+  }
+
+  console.log(finalAnswer);
+}
+
+// model
+//   .findAnswers(document.getElementById("questionInput").value, filteredData)
+//   .then((answers) => {
+//     const resultElement = document.querySelector(".result");
+//     resultElement.innerHTML = "";
+//     const ul = document.createElement("ul");
+//     answers.forEach((e) => {
+//       const li = document.createElement("li");
+//       li.innerText = e.text;
+//       ul.appendChild(li);
+//     });
+//     resultElement.appendChild(ul);
+//   });
+
+// .then((answers) => {
+//   const resultElement = document.querySelector(".result");
+//   resultElement.innerHTML = "";
+//   const ul = document.createElement("ul");
+//   answers.forEach((e) => {
+//     const li = document.createElement("li");
+//     li.innerText = e.text;
+//     ul.appendChild(li);
+//   });
+//   resultElement.appendChild(ul);
+// });
